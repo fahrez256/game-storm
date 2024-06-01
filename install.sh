ce90fa888fdf3184a3309b4569e8554aed9eea15
Fahrez256Bit <167403685+fahrez256@users.noreply.github.com>
2024-06-01 22:55:44 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,16 @@
 source $FUNCTION
 import axeron.prop
 local core="https://fahrez256.github.io/Laxeron/shell/core.sh"
+local id="$(settings get secure android_id)"
 
-echo "Your Id : $(settings get secure android_id)"
-echo "$name : test"
+case $1 in
+  --info )
+    echo "id : $id"
+    echo "vip : false"
+    ;;
+esac
+
+echo "$name : online"
 echo "Version : free"
 echo "Need more features? donate \$1"
 
