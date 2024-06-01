355e587855c7240af7c2275e7d0fb5fbfcf50426
Fahrez256Bit <167403685+fahrez256@users.noreply.github.com>
2024-06-01 21:07:44 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,3 @@
-if [ $AXERON = false ]; then
-  echo "Only support in LAxeron"
-  exit 999
-fi
-
 source $FUNCTION
 import axeron.prop
 local core="https://fahrez256.github.io/Laxeron/shell/core.sh"
@@ -10,3 +5,4 @@ local core="https://fahrez256.github.io/Laxeron/shell/core.sh"
 echo "$name : test"
 echo "Version : free"
 echo "Need more features? donate \$1"
+echo "Aldo Kontol"
