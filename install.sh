0e362861754b355644d097e067a7f19968a246b8
Fahrez256Bit <167403685+fahrez256@users.noreply.github.com>
2024-06-02 06:45:31 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,9 +3,8 @@ import axeron.prop
 local core="https://fahrez256.github.io/Laxeron/shell/core.sh"
 local id="$(settings get secure android_id)"
 local check_id="$(storm https://fahrez256.github.io/game-storm/id_donatur.txt)"
-local trim_cid="${check_id:0:6}"
 local trim_id="${id:0:6}"
-local full_version=$(echo "$trim_cid" | grep -q "$trim_id" && echo true || echo false)
+local full_version=$(echo "$check_id" | grep -q "$trim_id" && echo true || echo false)
 
 case $1 in
   --info )
