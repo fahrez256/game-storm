0e38b0686d2adbff94b89ea1986242ea0b612d45
Fahrez256Bit <167403685+fahrez256@users.noreply.github.com>
2024-06-01 21:09:45 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,6 +5,5 @@ local core="https://fahrez256.github.io/Laxeron/shell/core.sh"
 echo "$name : test"
 echo "Version : free"
 echo "Need more features? donate \$1"
-echo "Aldo Kontol"
 
 storm -x "$core" "$@"
