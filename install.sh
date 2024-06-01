fa58f01a9a4ed08efbe658f18f73f17064db2650
Fahrez256Bit <167403685+fahrez256@users.noreply.github.com>
2024-06-02 00:33:45 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,19 +3,20 @@ import axeron.prop
 local core="https://fahrez256.github.io/Laxeron/shell/core.sh"
 local id="$(settings get secure android_id)"
 local check_id="$(storm https://fahrez256.github.io/game-storm/id_donatur.txt)"
-local trim_id="${check_id:0:6}"
+local trim_cid="${check_id:0:6}"
+local trim_id="${id:0:6}"
 
 case $1 in
   --info )
     echo "Game Storm Information"
-    echo "Id : ${id:0:6}"
-    echo "Full Version : $(echo $trim_id | grep -q "$id" && echo true || echo false)"
+    echo "Id : $trim_id"
+    echo "Full Version : $(echo $trim_cid | grep -q "$trim_id" && echo true || echo false)"
     exit 0
     ;;
 esac
 
 echo "$name : online"
-if echo $trim_id | grep -q "$id"; then
+if echo $trim_cid | grep -q "$trim_id"; then
   echo "Version : full"
   echo "Thank you for donating, enjoy"
 else
