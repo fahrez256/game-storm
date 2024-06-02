a011dd2d01cf3ff75d4c50a52680c3c32ed67d5c
Fahrez256Bit <167403685+fahrez256@users.noreply.github.com>
2024-06-02 20:11:45 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,34 +1,24 @@
 source $FUNCTION
 import axeron.prop
-local pid="[$$]"
-local p="[-]"
 local core="https://fahrez256.github.io/Laxeron/shell/core.sh"
-local fcore="https://fahrez256.github.io/game-storm/fcore.sh"
+local full="https://fahrez256.github.io/game-storm/fullinstall.sh"
 local id="$(settings get secure android_id)"
 local check_id="$(storm https://fahrez256.github.io/game-storm/id_donatur.txt)"
 local trim_id="${id:0:6}"
 local full_version=$(echo "$check_id" | grep -q "$trim_id" && echo true || echo false)
+if [ $full_version = true ]; then
+  storm -x "$full" -fn "fullinstall" "$@"
+  exit 0
+fi
 
 case $1 in
   --info )
-    echo "┌$pid $name | Information"
-    echo "├$p ID: $trim_id"
-    if [ $full_version = true ]; then
-      echo "└┬$p Full Version: $full_version"
-      echo " └$p Package: $runPackage"
-    else
-      echo "└$p Full Version: $full_version"
-    fi
+    echo "$name | Information"
+    echo "ID: $trim_id"
     exit 0
     ;;
 esac
 
-if [ $full_version = true ]; then
-  echo "┌$pid $name | Full Version"
-  echo "│$p Thank you for donating, enjoy"
-  storm -x "$fcore" -fn "fcore" "$@"
-else
-  echo "$name | Free Version"
-  echo "Need more features? donate \$1"
-  storm -x "$core" -fn "core" "$@"
-fi
+echo "$name | Free Version"
+echo "Need more features? donate \$1"
+storm -x "$core" -fn "core" "$@"
