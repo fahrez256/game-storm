28679a5e2b0c0531849d1700755c4e9ef29b5175
Fahrez256Bit <167403685+fahrez256@users.noreply.github.com>
2024-06-06 23:32:16 +0700

    Update install.sh

--- full/install.sh
+++ full/install.sh
@@ -45,7 +45,7 @@ case $1 in
     log_file="${log_path}/log.txt"
     current_time=$(date +%s%3N)
     last_time=$(cat "$log_file" 2>/dev/null)
-    time_diff=$((current_time - last_time))
+    time_diff=$((last_time - current_time))
     converted_time=$(time_conv $time_diff "no cooldown")
     echo "┌$pid $name | Information"
     echo "├$p ID: $trim_id"
