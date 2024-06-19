b8fd90bfff56547f0f1076a0f8a37e0d8abd087d
Fahrez256Bit <167403685+fahrez256@users.noreply.github.com>
2024-06-19 15:22:40 +0700

    Update help.sh

--- full/help.sh
+++ full/help.sh
@@ -1,5 +1,5 @@
 echo "Game-Storm:
-Usage: ash gstorm [<options>|-p <package>]
+Usage: ash gstorm [-p <package>|<options>]
 Options:
   --help | -h : to print this
   --info | -i : to check modules info
